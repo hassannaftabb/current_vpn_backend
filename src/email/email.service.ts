0a62f09baf70d278636fd3fdb93ca29f3c0fdb38
Hassan <hassannaftabb@gmail.com>
2024-01-04 19:17:53 +0500

    email fix

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
-import axios from 'axios';
 import {
   generatePasswordResetConfirmation,
   generateResetPasswordOtpEmailTemplate,
@@ -25,48 +24,18 @@ export class EmailService {
     subject: string,
     text: string,
     html?: string,
-    name?: string,
   ): Promise<void> {
     const mailOptions: nodemailer.SendMailOptions = {
-      from: `"Current VPN" ${process.env.SUBTITLEO_SUPPORT_EMAIL}`,
+      from: `"Current VPN" ${process.env.CURRENTVPN_SUPPORT_EMAIL}`,
       to: to,
       subject: subject,
       text: text,
       html: html,
     };
 
-    this.transporter.sendMail(mailOptions, async (error, info) => {
+    this.transporter.sendMail(mailOptions, async (error) => {
       if (error) {
         console.error('Failed to send email:', error);
-      } else {
-        console.log(name, name.split(' ')[0], name.split(' ')[1]);
-        await axios
-          .post(
-            'https://api.brevo.com/v3/contacts',
-            {
-              email: to,
-              listIds: [2],
-              updateEnabled: false,
-              attributes: {
-                FIRSTNAME: name.split(' ')[0],
-                LASTNAME: name.split(' ')[1],
-              },
-            },
-            {
-              headers: {
-                accept: 'application/json',
-                'content-type': 'application/json',
-                'api-key': process.env.BREVO_API_KEY,
-              },
-            },
-          )
-          .then((response) => {
-            console.log(response.data);
-            console.log('Email sent successfully:', info.response);
-          })
-          .catch((error) => {
-            console.log('Coudnt cra=eate contact', error);
-          });
       }
     });
   }
@@ -79,7 +48,7 @@ export class EmailService {
     const subject = 'Reset Password OTP Confirmation';
     const text = `Welcome to Subtitleo, ${name}! Here is your OTP to reset your password: ${otp}. It'll expire in 10 minutes. Happy subtitling! Best regards, The Subtitleo Team`;
     const html = generateResetPasswordOtpEmailTemplate(name, otp);
-    await this.sendEmail(to, subject, text, html, name);
+    await this.sendEmail(to, subject, text, html);
   }
   async sendPasswordResetConfirmationEmail(
     to: string,
@@ -88,6 +57,6 @@ export class EmailService {
     const subject = 'Password reset successfull!';
     const text = `Your password have been resetted successfully!`;
     const html = generatePasswordResetConfirmation(name);
-    await this.sendEmail(to, subject, text, html, name);
+    await this.sendEmail(to, subject, text, html);
   }
 }
